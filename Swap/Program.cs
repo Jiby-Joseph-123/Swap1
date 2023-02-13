653d656f27cdf98dbe6e1ae2ae2a219b403b0b0e
Jiby Joseph <jiby.joseph@experionglobal.local>
2023-02-13 15:13:28 +0530

    Swapping

---
+++ Swap/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            Console.WriteLine(" Before swapping a=" + a);
+            Console.WriteLine("Brfore swapping b=" +b );
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine(" After swapping a="+ a);
+            Console.WriteLine("After swapping b=" + b );
+
+        }
+    }
+}
