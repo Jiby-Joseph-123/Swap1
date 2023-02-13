30194a4ff91a523893a762845d9fac8be02ab058
jiby.joseph <jibyjoseph0996@gmail.com>
2023-02-13 16:27:03 +0530

    second

--- Swap/Program.cs
+++ Swap/Program.cs
@@ -6,13 +6,13 @@
         {
             int a = 10;
             int b = 20;
-            Console.WriteLine(" Before swapping a=" + a);
-            Console.WriteLine("Brfore swapping b=" +b );
+            Console.WriteLine(" Before swapping a: " + a);
+            Console.WriteLine("Brfore swapping b: " +b );
             int temp = a;
             a = b;
             b = temp;
-            Console.WriteLine(" After swapping a="+ a);
-            Console.WriteLine("After swapping b=" + b );
+            Console.WriteLine(" After swapping a: "+ a);
+            Console.WriteLine("After swapping b: " + b );
 
         }
     }
